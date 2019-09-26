fc32fbbac385fcbba33b254ec05157ac4edf1eb3
salah_yousef <salah.yousef@pharmatolia.com>
2019-09-26 19:24:53 +0300

    use mockapi

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-if (localStorage.getItem('studyToggle') === null) {
-    localStorage.setItem('studyToggle', 'free');
-} else {
-    $( "input[type='radio']" ).change(function() {
-        localStorage.setItem('studyToggle', $('input[name=inlineRadioOptions]:checked').val());
+$( "input[type='radio']" ).change(function() {
+    console.log('yyy');
+    localStorage.setItem('studyToggle', $('input[name=inlineRadioOptions]:checked').val());
+    $.ajax({
+        url: 'http://5d8cd857443e3400143b4b9c.mockapi.io/api/user/1',    //Your api url
+        type: 'PUT',   //type is any HTTP method
+        data: {
+                "id": "1",
+                "studyToggle": $('input[name=inlineRadioOptions]:checked').val()
+            
+        },      //Data as js object
+        success: function () {
+            console.log('success');
+        }
     });
-    switch (localStorage.getItem('studyToggle')) {
-        case 'Salah':
-            $("#Salah").prop("checked", true);
-            break;
-        case 'Mahmoud':
-            $("#Mahmoud").prop("checked", true);
-            break;
-        default:
-            $("#Free").prop("checked", true);
-            break;
-    }
-}
+    
+});
 
+$.ajax({
+    url: "http://5d8cd857443e3400143b4b9c.mockapi.io/api/user/1",
+    type: 'GET',
+    success: function(res) {
+        console.log(res.studyToggle);
+        switch (res.studyToggle) {
+            case 'Salah':
+                $("#Salah").prop("checked", true);
+                break;
+            case 'Mahmoud':
+                $("#Mahmoud").prop("checked", true);
+                break;
+            default:
+                $("#Free").prop("checked", true);
+                break;
+        }
+    }
+});
